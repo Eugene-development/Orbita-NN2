d6ab375a383db036871e69614183f9d91b1161f9
indexpro24@gmail.com <indexpro24@gmail.com>
2021-06-08 21:18:24 +0300

    git init

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -38,6 +38,58 @@
     </div>
 
 
+
+    <div class="bg-white overflow-hidden">
+      <div class="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+        <div class="hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen"></div>
+        <div class="mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none">
+          <div>
+            <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">Строительная база</h2>
+            <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Орбита-строй</h3>
+          </div>
+        </div>
+        <div class="mt-8 lg:grid lg:grid-cols-2 lg:gap-8">
+          <div class="relative lg:row-start-1 lg:col-start-2">
+            <svg class="hidden lg:block absolute top-0 right-0 -mt-20 -mr-20" width="404" height="384" fill="none" viewBox="0 0 404 384" aria-hidden="true">
+              <defs>
+                <pattern id="de316486-4a29-4312-bdfc-fbce2132a2c1" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                  <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                </pattern>
+              </defs>
+              <rect width="404" height="384" fill="url(#de316486-4a29-4312-bdfc-fbce2132a2c1)" />
+            </svg>
+            <div class="relative text-base mx-auto max-w-prose lg:max-w-none">
+              <figure>
+                <div class="aspect-w-12 aspect-h-7 lg:aspect-none">
+                  <img class="rounded-lg shadow-lg object-cover object-center" src="https://lumen-image-bucket.s3.eu-central-1.amazonaws.com/pages/serv_4.jpg" alt="stroymat" width="1184" height="1376">
+                </div>
+              </figure>
+            </div>
+          </div>
+          <div class="mt-8 lg:mt-0">
+            <div class="mt-1 prose prose-indigo text-gray-500 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1">
+              <p>Приобрести стройматериалы в Нижнем Новгороде высокого качества по конкурентным ценам можно в течение нескольких минут. База строительных и отделочных материалов «Орбита-Строй» занимает площадь более, чем 1 км2 и предлагает продукцию от таких производителей, как:</p>
+              <ul>
+                <li>Tarkett;</li>
+                <li>EK;</li>
+                <li>Ceresit;</li>
+                <li>Makroflex;</li>
+              </ul>
+              <p>Строительная база имеет удобный подъезд автотранспорта, развитую инфраструктуру и просторные выставочные павильоны. Строительный магазин предоставляет услуги по погрузке при оптовой покупке на бесплатных условиях, возможна доставка покупки собственным автотранспортом.</p>
+              <p>Строительный интернет-магазин подойдёт тем, кто ценит время и качество. Выбрать товар заблаговременно оплатить покупку стройматериалов в Нижнем Новгороде и заказать их доставку по указанному адресу – дело нескольких минут. Стройбаза идёт навстречу постоянным клиентам и предоставляет гибкие дисконтные скидки.</p>
+              <p>«Орбита-Строй» - строительный магазин в Нижнем Новгороде для тех, кто уважает качество материалов и труд. «Орбита-Строй» - правильный выбор на пути к комфорту и надёжности.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
     <div class="mt-20 max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="lg:text-center">
         <p class="text-base leading-6 text-teal-800 font-semibold tracking-wide uppercase"></p>
@@ -157,35 +209,3 @@ export default {
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-  'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
