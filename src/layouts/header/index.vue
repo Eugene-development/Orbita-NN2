3207215ee299cdf102ebdfa85e186935a6497625
indexpro24@gmail.com <indexpro24@gmail.com>
2021-06-28 14:57:29 +0300

    git init

--- src/layouts/header/index.vue
+++ src/layouts/header/index.vue
@@ -660,56 +660,14 @@
           <div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="w-full mx-auto">
               <dl class="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-6">
-                <div class="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
+                <div
+                  v-for="(head, idx) of allHead" :key="allHead.id"
+                  class="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
                   <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-                    Стройматериалы
+                    {{ head.name }}
                   </dt>
                   <dd class="order-1 text-5xl font-extrabold text-indigo-600">
                     86
-                  </dd>
-                </div>
-                <div
-                  class="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
-                  <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-                    Отделка
-                  </dt>
-                  <dd class="order-1 text-5xl font-extrabold text-indigo-600">
-                    198
-                  </dd>
-                </div>
-                <div
-                  class="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
-                  <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-                    Инструмент
-                  </dt>
-                  <dd class="order-1 text-5xl font-extrabold text-indigo-600">
-                    100
-                  </dd>
-                </div>
-                <div
-                  class="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
-                  <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-                    Электротовары
-                  </dt>
-                  <dd class="order-1 text-5xl font-extrabold text-indigo-600">
-                    100
-                  </dd>
-                </div>
-                <div
-                  class="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
-                  <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-                    Изоляция
-                  </dt>
-                  <dd class="order-1 text-5xl font-extrabold text-indigo-600">
-                    24
-                  </dd>
-                </div>
-                <div class="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
-                  <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-                    Сантехника
-                  </dt>
-                  <dd class="order-1 text-5xl font-extrabold text-indigo-600">
-                    25
                   </dd>
                 </div>
               </dl>
