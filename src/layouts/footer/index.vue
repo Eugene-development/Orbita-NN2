4fbd8d80155b6c70992b9e17ccf89707697cacbd
indexpro24@gmail.com <indexpro24@gmail.com>
2022-02-13 19:59:09 +0300

    git init

--- src/layouts/footer/index.vue
+++ src/layouts/footer/index.vue
@@ -47,9 +47,9 @@
                     <svg class="my-auto mr-2 text-red-800 w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                     </svg>
-                    orbita88yuliya@yandex.ru
+                    info@orbita-stroy.com
                   </p>
-                    <a class="  flex" href="https://vk.com/orbita_stroy"
+                    <a class="flex" href="https://vk.com/orbita_stroy"
                        target="_blank">
                       <svg class="my-auto mr-2 text-red-800 w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
